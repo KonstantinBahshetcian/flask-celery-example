6d8df0e6b8992f7641535544cc57b7b782349a62
KonstantinBahshetcian <55201971+KonstantinBahshetcian@users.noreply.github.com>
2019-12-07 21:06:17 +0500

    Update test_hello_world.py

--- test_hello_world.py
+++ test_hello_world.py
@@ -1,17 +1,17 @@
-import app
 import unittest
-
-class TestHelloWorld(unittest.TestCase):
-
-    def setUp(self):
-        self.app = app.app.test_client()
-        self.app.testting = True
-
-    def test_statsu_code(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_greeting_message(self):
-
-if __name__ == '__main__':
-    unnitest.main()
+import xmlrunner
+    
+    
+tests = unittest.TestLoader().discover('tests')
+# run tests with unittest-xml-reporting and output to $CIRCLE_TEST_REPORTS on CircleCI or test-reports locally
+xmlrunner.XMLTestRunner(output=os.environ.get('CIRCLE_TEST_REPORTS','test-reports')).run(tests)
+if COV:
+    COV.stop()
+    COV.save()
+    print('Coverage Summary:')
+    COV.report()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    covdir = os.path.join(basedir, 'tmp/coverage')
+    COV.html_report(directory=covdir)
+    print('HTML version: file://%s/index.html' % covdir)
+    COV.erase()
